e71aa4acad70873e68fefd702460286d0b5a5c0e
radzni <115959454+radzni@users.noreply.github.com>
2025-02-18 12:39:48 +0800

    Delete ACT2.php

--- ACT2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$fruits = array("Apple", "Banana", "Orange", "Grapes", "Mango");
-
-echo "<ol>";
-for ($i = 0; $i < count($fruits); $i++) {
-    echo "<li>" . $fruits[$i] . "</li>";
-}
-echo "</ol>";
-?>
